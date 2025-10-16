869bb72352d6d4f86e6a878798378b206715f2b6
Frederic Guyont <you@example.com>
2025-10-16 16:17:48 +0200

    ADD dbcontext and dbset

---
+++ PostIt.Database/PostItDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PostIt.Database.EntityModels;
+
+namespace PostIt.Database
+{
+    public class PostItDbContext : DbContext
+    {
+        public PostItDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public virtual DbSet<User> Users { get; set; }
+    }
+}
