f18f950397e5803d8b39c3a5c5e48f0b6616bd80
Frederic Guyont <you@example.com>
2025-10-19 18:40:07 +0200

    ADD dbsets and onmodelcreating

--- PostIt.Database/PostItDbContext.cs
+++ PostIt.Database/PostItDbContext.cs
@@ -10,5 +10,32 @@ namespace PostIt.Database
         }
 
         public virtual DbSet<User> Users { get; set; }
+
+        public virtual DbSet<Post> Posts { get; set; }
+
+        public virtual DbSet<Subject> Subjects { get; set; }
+
+        public virtual DbSet<Comment> Comments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.Subjects)
+                .WithMany(s => s.Users)
+                .UsingEntity(j => j.ToTable("Subscriptions"));
+
+            modelBuilder.Entity<Comment>()
+                .HasOne<Post>(c => c.Post)
+                .WithMany(p => p.Comments)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Comment>()
+                .HasOne<User>(c => c.User)
+                .WithMany(u => u.Comments)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
     }
 }
