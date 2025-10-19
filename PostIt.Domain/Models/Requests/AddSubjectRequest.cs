c6b477689184515b3623461f02fad2bb7d99da6e
Frederic Guyont <you@example.com>
2025-10-19 22:19:18 +0200

    ADD addsubjectrequest

---
+++ PostIt.Domain/Models/Requests/AddSubjectRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostIt.Domain.Models.Requests
+{
+    public class AddSubjectRequest
+    {
+        [Required]
+        [MaxLength(50)]
+        public required string Name { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public required string Description { get; set; }
+    }
+}
