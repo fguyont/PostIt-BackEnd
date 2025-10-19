0dc6ea1bee47ca9d7dd05a132b69c49462dc236a
Frederic Guyont <you@example.com>
2025-10-19 17:49:09 +0200

    ADD usercontroller

---
+++ PostIt.API/Controllers/UserController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PostIt.Domain.Interfaces.IBusiness;
+using PostIt.Domain.Models.Responses;
+using System.Security.Claims;
+
+namespace PostIt.API.Controllers
+{
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserBusiness _userBusiness;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public UserController(IUserBusiness userBusiness, IHttpContextAccessor httpContextAccessor)
+        {
+            _userBusiness = userBusiness;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        [Authorize]
+        [HttpGet]
+        [Route("[controller]/{id}")]
+        public async Task<ActionResult> GetUserById(long id)
+        {
+            GetUserResponse? getUserResponse = await _userBusiness.GetUserById(id);
+
+            if (getUserResponse != null)
+            {
+                return Ok(getUserResponse);
+            }
+            return BadRequest(new { message = "User not found." });
+        }
+
+        [Authorize]
+        [HttpGet]
+        [Route("[controller]/me")]
+        public async Task<ActionResult> GetConnectedUser()
+        {
+            string? userEmail = _httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.Email);
+
+            if (userEmail != null)
+            {
+                GetUserResponse? getUserResponse = await _userBusiness.GetUserByEmail(userEmail);
+                if (getUserResponse != null)
+                {
+                    return Ok(getUserResponse);
+                }
+                return BadRequest(new { message = "Connected user not found." });
+            }
+            return BadRequest(new { message = "Connected user not found." });
+        }
+    }
+}
