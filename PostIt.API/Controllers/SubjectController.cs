d6417b8d76912fcdfd90ca6f6e3fd72d3136e2d5
Frederic Guyont <you@example.com>
2025-10-19 22:21:03 +0200

    ADD create subject and get all actions

---
+++ PostIt.API/Controllers/SubjectController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PostIt.Domain.Interfaces.IBusiness;
+using PostIt.Domain.Models;
+using PostIt.Domain.Models.Requests;
+
+namespace PostIt.API.Controllers
+{
+    [ApiController]
+    public class SubjectController : ControllerBase
+    {
+        private readonly ISubjectBusiness _subjectBusiness;
+
+        public SubjectController(ISubjectBusiness subjectBusiness)
+        {
+            _subjectBusiness = subjectBusiness;
+        }
+
+        [Authorize]
+        [HttpGet]
+        [Route("[controller]/all")]
+        public IActionResult GetAllSubjects()
+        {
+            List<SubjectModel> subjects = new List<SubjectModel>();
+            subjects = _subjectBusiness.GetAllSubjects();
+
+            return Ok(subjects);
+        }
+
+        [Authorize]
+        [HttpPost]
+        [Route("[controller]/new")]
+        public async Task<IActionResult> CreateSubject([FromBody] AddSubjectRequest addSubjectRequest)
+        {
+            SubjectModel subjectAdded = await _subjectBusiness.CreateSubject(addSubjectRequest);
+
+            if (subjectAdded != null)
+            {
+                return Ok(subjectAdded);
+            }
+            return BadRequest(new { message = "Subject creation failed."});
+        }
+    }
+}
