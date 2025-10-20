4b80904b9f7c38248f71a770484549477dc6bd50
Frederic Guyont <you@example.com>
2025-10-20 23:21:04 +0200

    ADD sub/unsub actions

--- PostIt.API/Controllers/SubjectController.cs
+++ PostIt.API/Controllers/SubjectController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 using PostIt.Domain.Interfaces.IBusiness;
 using PostIt.Domain.Models;
 using PostIt.Domain.Models.Requests;
+using PostIt.Domain.Models.Responses;
+using System.Security.Claims;
 
 namespace PostIt.API.Controllers
 {
@@ -10,21 +12,40 @@ namespace PostIt.API.Controllers
     public class SubjectController : ControllerBase
     {
         private readonly ISubjectBusiness _subjectBusiness;
+        private readonly IUserBusiness _userBusiness;
+        private readonly IHttpContextAccessor _httpContextAccessor;
 
-        public SubjectController(ISubjectBusiness subjectBusiness)
+        public SubjectController(ISubjectBusiness subjectBusiness, IUserBusiness userBusiness, IHttpContextAccessor httpContextAccessor)
         {
             _subjectBusiness = subjectBusiness;
+            _userBusiness = userBusiness;
+            _httpContextAccessor = httpContextAccessor;
         }
 
         [Authorize]
         [HttpGet]
         [Route("[controller]/All")]
-        public IActionResult GetAllSubjects()
+            public IActionResult GetAllSubjects()
         {
             List<SubjectModel> subjects = new List<SubjectModel>();
             subjects = _subjectBusiness.GetAllSubjects();
 
             return Ok(subjects);
+        }
+
+        [Authorize]
+        [HttpGet]
+        [Route("[controller]/{id}")]
+        public async Task<ActionResult> GetPostById(long id)
+        {
+            SubjectModel? subject = await _subjectBusiness.GetSubjectById(id);
+
+            if (subject != null)
+            {
+                return Ok(subject); ;
+            }
+
+            return BadRequest(new { message = "Subject not found." });
         }
 
         [Authorize]
@@ -39,6 +60,78 @@ namespace PostIt.API.Controllers
                 return Ok(subjectCreated);
             }
             return BadRequest(new { message = "Subject creation failed."});
+        }
+
+        [Authorize]
+        [HttpPost]
+        [Route("[controller]/{subjectId}/Subscribe")]
+        public async Task<IActionResult> Subscribe(long subjectId)
+        {
+            SubjectModel? subject = await _subjectBusiness.GetSubjectById(subjectId);
+
+            if (subject == null)
+            {
+                return BadRequest(new { message = "Subject not found." });
+            }
+
+            string? userEmail = _httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.Email);
+
+            if (userEmail == null)
+            {
+                return BadRequest(new { message = "Connected user not found." });
+            }
+
+            GetUserResponse? getUserResponse = await _userBusiness.GetUserByEmail(userEmail);
+
+            if (getUserResponse == null)
+            {
+                return BadRequest(new { message = "Connected user not found." });
+            }
+
+            UserSubjectSuccess subSuccess = await _subjectBusiness.Subscribe(subjectId, getUserResponse.Id);
+
+            if (subSuccess != null)
+            {
+                return Ok(subSuccess);
+            }
+
+            return BadRequest(new { message = "Subscription failed." });
+        }
+
+        [Authorize]
+        [HttpPost]
+        [Route("[controller]/{subjectId}/Unsubscribe")]
+        public async Task<IActionResult> Unsubscribe(long subjectId)
+        {
+            SubjectModel? subject = await _subjectBusiness.GetSubjectById(subjectId);
+
+            if (subject == null)
+            {
+                return BadRequest(new { message = "Subject not found." });
+            }
+
+            string? userEmail = _httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.Email);
+
+            if (userEmail == null)
+            {
+                return BadRequest(new { message = "Connected user not found." });
+            }
+
+            GetUserResponse? getUserResponse = await _userBusiness.GetUserByEmail(userEmail);
+
+            if (getUserResponse == null)
+            {
+                return BadRequest(new { message = "Connected user not found." });
+            }
+
+            UserSubjectSuccess unsubSuccess = await _subjectBusiness.Unsubscribe(subjectId, getUserResponse.Id);
+
+            if (unsubSuccess != null)
+            {
+                return Ok(unsubSuccess);
+            }
+
+            return BadRequest(new { message = "Unsubscription failed." });
         }
     }
 }
