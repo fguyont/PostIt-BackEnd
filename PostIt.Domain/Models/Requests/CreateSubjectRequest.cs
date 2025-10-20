7da46fd3631f92c7220e64e98fef672f07233b55
Frederic Guyont <you@example.com>
2025-10-20 14:21:35 +0200

    ADD createsubjectrequest model replacing addsubjectrequest

---
+++ PostIt.Domain/Models/Requests/CreateSubjectRequest.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PostIt.Domain.Models.Requests
+{
+    public class CreateSubjectRequest
+    {
+        [Required]
+        [MaxLength(50)]
+        public required string Name { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public required string Description { get; set; }
+    }
+}
