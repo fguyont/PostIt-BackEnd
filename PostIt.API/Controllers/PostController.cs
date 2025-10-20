755ab7a5132965c089bd54a609740ffeb1277bdf
Frederic Guyont <you@example.com>
2025-10-20 17:52:56 +0200

    ADD getpostbyid and misc fix

--- PostIt.API/Controllers/PostController.cs
+++ PostIt.API/Controllers/PostController.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using PostIt.Database.EntityModels;
 using PostIt.Domain.Interfaces.IBusiness;
 using PostIt.Domain.Models;
 using PostIt.Domain.Models.Requests;
 using PostIt.Domain.Models.Responses;
 using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace PostIt.API.Controllers
 {
@@ -12,12 +14,14 @@ namespace PostIt.API.Controllers
     public class PostController : ControllerBase
     {
         private readonly IPostBusiness _postBusiness;
+        private readonly ISubjectBusiness _subjectBusiness;
         private readonly IUserBusiness _userBusiness;
         private readonly IHttpContextAccessor _httpContextAccessor;
 
-        public PostController(IPostBusiness postBusiness, IUserBusiness userBusiness, IHttpContextAccessor httpContextAccessor)
+        public PostController(IPostBusiness postBusiness, ISubjectBusiness subjectBusiness, IUserBusiness userBusiness, IHttpContextAccessor httpContextAccessor)
         {
             _postBusiness = postBusiness;
+            _subjectBusiness = subjectBusiness;
             _userBusiness = userBusiness;
             _httpContextAccessor = httpContextAccessor;
         }
@@ -25,8 +29,15 @@ namespace PostIt.API.Controllers
         [Authorize]
         [HttpGet]
         [Route("Subject/{subjectId}/[controller]/All")]
-        public IActionResult GetAllPosts(long subjectId)
+        public async Task<IActionResult> GetAllPosts(long subjectId)
         {
+            SubjectModel? subject = await _subjectBusiness.GetSubjectById(subjectId);
+
+            if (subject == null)
+            {
+                return BadRequest(new { message = "Subject not found." });
+            }
+
             List<PostModel> posts = new List<PostModel>();
             posts = _postBusiness.GetAllPosts(subjectId);
 
@@ -34,10 +45,38 @@ namespace PostIt.API.Controllers
         }
 
         [Authorize]
+        [HttpGet]
+        [Route("Subject/{subjectId}/[controller]/{postId}")]
+        public async Task<ActionResult> GetPostById(long postId, long subjectId)
+        {
+            SubjectModel? subject = await _subjectBusiness.GetSubjectById(subjectId);
+
+            if (subject == null)
+            {
+                return BadRequest(new { message = "Subject not found." });
+            }
+
+            PostModel? postModel = await _postBusiness.GetPostById(postId);
+
+            if (postModel != null)
+            {
+                return Ok(postModel);
+            }
+            return BadRequest(new { message = "Post not found." });
+        }
+
+        [Authorize]
         [HttpPost]
         [Route("Subject/{subjectId}/[controller]/New")]
         public async Task<IActionResult> CreatePost([FromBody] CreatePostRequest createPostRequest, long subjectId)
         {
+            SubjectModel? subject = await _subjectBusiness.GetSubjectById(subjectId);
+
+            if (subject == null)
+            {
+                return BadRequest(new { message = "Subject not found." });
+            }
+
             string? userEmail = _httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.Email);
 
             if (userEmail == null)
@@ -64,9 +103,16 @@ namespace PostIt.API.Controllers
 
         [Authorize]
         [HttpPut]
-        [Route("Subject/{subjectId}/[controller]/{postId}/Update")]
-        public async Task<IActionResult> UpdatePost([FromBody] CreatePostRequest createPostRequest, long postId)
+        [Route("Subject/{subjectId}/[controller]/{postId}/Edit")]
+        public async Task<IActionResult> UpdatePost([FromBody] CreatePostRequest createPostRequest, long postId, long subjectId)
         {
+            SubjectModel? subject = await _subjectBusiness.GetSubjectById(subjectId);
+
+            if (subject == null)
+            {
+                return BadRequest(new { message = "Subject not found." });
+            }
+
             PostModel? postToUpdate = await _postBusiness.GetPostById(postId);
 
             if (postToUpdate == null)
@@ -105,9 +151,16 @@ namespace PostIt.API.Controllers
 
         [Authorize]
         [HttpPut]
-        [Route("Subject/{subjectId}/[controller]/{postId}/Unactivate")]
-        public async Task<IActionResult> UnactivatePost(long postId)
+        [Route("Subject/{subjectId}/[controller]/{postId}/Remove")]
+        public async Task<IActionResult> UnactivatePost(long postId, long subjectId)
         {
+            SubjectModel? subject = await _subjectBusiness.GetSubjectById(subjectId);
+
+            if (subject == null)
+            {
+                return BadRequest(new { message = "Subject not found." });
+            }
+
             PostModel? postToUnactivate = await _postBusiness.GetPostById(postId);
 
             if (postToUnactivate == null)
