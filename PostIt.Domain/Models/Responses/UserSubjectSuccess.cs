d8a201348f037f7cdc09c68fa0bf355c56f1e81a
Frederic Guyont <you@example.com>
2025-10-20 23:20:11 +0200

    ADD usersubjectsuccess for sub/unsub responses

---
+++ PostIt.Domain/Models/Responses/UserSubjectSuccess.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PostIt.Domain.Models.Responses
+{
+    public class UserSubjectSuccess
+    {
+        [Required]
+        public long UserId { get; set; }
+
+        [Required]
+        public required string UserName { get; set; }
+
+        [Required]
+        public long SubjectId { get; set; }
+
+        [Required]
+        public required string SubjectName { get; set; }
+    }
+}
