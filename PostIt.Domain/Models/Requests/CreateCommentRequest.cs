a51c93a495d66a3b86fd16d2d117006e2c619802
Frederic Guyont <you@example.com>
2025-10-20 17:53:24 +0200

    ADD createcommentrequest model

---
+++ PostIt.Domain/Models/Requests/CreateCommentRequest.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PostIt.Domain.Models.Requests
+{
+    public class CreateCommentRequest
+    {
+        [Required]
+        [MaxLength(2000)]
+        public required string Text { get; set; }
+    }
+}
