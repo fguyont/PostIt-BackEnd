316434b6e7b7d0808bbd23219386020948b0ba12
Frederic Guyont <you@example.com>
2025-10-20 14:21:51 +0200

    ADD createpostrequest

---
+++ PostIt.Domain/Models/Requests/CreatePostRequest.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PostIt.Domain.Models.Requests
+{
+    public class CreatePostRequest
+    {
+        [Required]
+        [MaxLength(50)]
+        public required string Title { get; set; }
+
+        [Required]
+        [MaxLength(5000)]
+        public required string Text { get; set; }
+    }
+}
